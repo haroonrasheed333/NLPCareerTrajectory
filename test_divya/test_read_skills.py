ab7d567ff298397b38e3201774c0767c0887328e
DivyaKarthikeyan <divimit@gmail.com>
2014-04-13 13:07:12 -0700

    adding more tests to skills extraction

--- test_divya/test_read_skills.py
+++ test_divya/test_read_skills.py
@@ -23,3 +23,11 @@ def test_skills_dict_value_must_be_from_skils_file_for_title():
 		skill_added = 1
         eq_(1,skill_added)
 
+
+def test_add_skills_only_if_file_in_json():
+
+	training_data =[("Responsible for developing and implementing a high level security feature in the homepage","sales manager", "1_plaintext.txt")]
+        expected_value = 0
+        skills_dict = read_skills_from_json_file(training_data)
+        
+        eq_(expected_value,len(skills_dict))
