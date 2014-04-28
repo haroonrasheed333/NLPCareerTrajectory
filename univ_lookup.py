408c4775511fa255b9871e7c76ccb894b18e22e9
DivyaKarthikeyan <divimit@gmail.com>
2014-04-27 17:13:26 -0700

    extracting univ name using ngram

--- univ_lookup.py
+++ univ_lookup.py
@@ -8,32 +8,63 @@ import csv
 import json
 import ngram
 from ngram import NGram
+import nltk
+#from iHire import extract_text_from_pdf
 
+
+punct = re.compile(r'^[^A-Za-z0-9]+|[^a-zA-Z0-9]+$')
+is_word=re.compile(r'[a-z]', re.IGNORECASE)
+sentence_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+word_tokenizer=nltk.tokenize.punkt.PunktWordTokenizer()
+
+def get_words(sentence):
+    return [punct.sub('',word) for word in word_tokenizer.tokenize(sentence) if is_word.search(word)]
+
+def ngrams(text, n):
+    for sentence in sentence_tokenizer.tokenize(text.lower()):
+        words = get_words(sentence)
+        for i in range(len(words)-(n-1)):
+            yield(' '.join(words[i:i+n]))
 
 def extract_univ(data):
     data = stripxml(str(data))
     data = data.lower()
-    # print data
+    data=data.replace('\xc2\xa0', ' ')
+    print data
     data = re.sub ('[^A-Za-z0-9 ]+',' ',str(data))
     data = re.sub ('  ',' ',str(data))
-    # print data
-
     if 'education' in data:
         parted = data.split('education')[1]
         second = parted[:150]
     else:
         second = data
-    with open("static/UniqueFBUnivNames.csv", 'rb') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            row = re.sub('[^A-Za-z0-9 ]+', ' ', str(row))
-            row = re.sub('  ', ' ', str(row))
-            if str(row).lower() in second:
-                if len(str(row).split())>1:
-                    return str(row)
-                    break
+    univ_dict = json.loads(open("static/univs_list.json","rb").read())
+    n=10
+    while (n>1):
+        for ngram in ngrams(str(second).lower(), n):
+            if ngram.lower() in univ_dict:
+                return ngram.title()
+        n = n - 1
+
+    #with open("static/UniqueFBUnivNames.csv", 'rb') as f:
+    #    reader = csv.reader(f)
+    #    for row in reader:
+    #        row = re.sub('[^A-Za-z0-9 ]+', ' ', str(row))
+    #        row = re.sub('  ', ' ', str(row))
+    #        if str(row).lower() in second:
+    #            if len(str(row).split())>1:
+    #                return str(row)
+    #                break
     return ""
 
+def extract_univ_json(data):
+    univ_dict = json.loads(open("static/univs_list.json","rb").read())
+    n=10
+    while (n>1):
+        for ngram in ngrams(str(data).lower(), n):
+            print ngram
+        n = n - 1
+    #print "curry college" in univ_dict
 
 def extract_from_resume(data):
     out = {}
@@ -79,7 +110,13 @@ def generate_ngrams(res):
     result = []
     for n in range(2,6):
         out[n] = zip(*[tokens[i:] for i in range(n)])
+
     return out
 
 if __name__ == '__main__':
-    extract_univ("Berekeley")
+    #extract_univ("Berekeley")
+    #data = open('/Users/divyakarthikeyan/Downloads/39-bc5e01a4f80acbcc3b0f1a92c1c7cd1a_CPhamDesignResume.pdf').
+    data = extract_text_from_pdf('/Users/divyakarthikeyan/Downloads/39-bc5e01a4f80acbcc3b0f1a92c1c7cd1a_CPhamDesignResume.pdf')
+    print data
+    print "---"
+    print extract_univ(data)
